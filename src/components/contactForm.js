2509e07b492901a22febc960221c3f22630c67ce
Phil Barber <phil@lantum.com>
2020-02-15 17:16:19 +0000

    [contact-form] Style the form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,7 +1,45 @@
 import React from 'react'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
+import styled from 'styled-components'
 import * as Yup from 'yup'
-import { TextInput } from './formik/textInput'
+import { TextInput, Label, Input, Error } from './formik/textInput'
+
+const TextInputStyled = styled(TextInput)`
+  ${Label} {
+    display: block;
+  }
+`
+
+const FullWidthInput = styled(TextInputStyled)`
+  ${Input} {
+    width: 100%;
+  }
+`
+
+const ShortInput = styled(TextInputStyled)`
+  width: 50%;
+`
+
+const MessageInput = styled(FullWidthInput)`
+  ${Input} {
+    height: 200px;
+  }
+`
+
+const FlexBox = styled.div`
+  display: flex;
+`
+
+const SubmitButton = styled.button`
+  flex-grow: 1;
+`
+
+const RequiredText = styled.div`
+  font-size: 12px;
+  flex-grow: 1;
+  text-align: right;
+  
+`
 
 export const ContactForm = () => {
   const bodyLimit = 300
@@ -31,35 +69,41 @@ export const ContactForm = () => {
     }}
   >
     {formik => (
-      <Form>
-        <TextInput
-          label="Email Address"
-          name="email"
-          type="email"
-        />
-        <TextInput
-          label="Name"
-          name="name"
-          type="text"
-        />
-        <TextInput
-          label="Company"
-          name="company"
-          type="text"
-        />
-        <TextInput
-          label="Message"
-          name="body"
-          textarea
-          placeholder={`Hi Phil,
+      <>
+        <Form>
+          <FullWidthInput
+            label="Email Address*"
+            name="email"
+            type="email"
+          />
+            <FlexBox>
+          <ShortInput
+            label="Name*"
+            name="name"
+            type="text"
+          />
+          <ShortInput
+            label="Company"
+            name="company"
+            type="text"
+          />
+          </FlexBox>
+          <MessageInput
+            label="Message*"
+            name="body"
+            textarea
+            placeholder={`Hi Phil,
 
-  Hope you're having a great day! 
+    Hope you're having a great day!
 
-  Listen to this really exciting project you could contribute to:
-  ...`}
-        />
-        <button type="submit" disabled={formik.isSubmitting}>Submit</button>
-      </Form>
+    Listen to this really exciting project you could contribute to:`}
+          />
+          <FlexBox>
+            <SubmitButton type="submit" disabled={formik.isSubmitting}>Submit</SubmitButton>
+            <RequiredText>*Required field</RequiredText>
+          </FlexBox>
+        </Form>
+      </>
     )}
   </Formik>
 };
