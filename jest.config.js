47949044e218605b0f8fec057eddae29cfab1351
Phil Barber <phil@lantum.com>
2020-07-03 09:32:47 +0100

    Jest and typescript

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   transform: {
     '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  moduleFileExtensions: ['ts', 'js'],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
@@ -10,9 +12,12 @@ module.exports = {
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
     __PATH_PREFIX__: ``,
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
   },
   testURL: `http://localhost`,
   setupFiles: [`<rootDir>/loadershim.js`],
-  setupFilesAfterEnv: ['<rootDir>src/setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>src/setupTests.ts'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
 };
