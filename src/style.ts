dafd99766f74a77d9e39332f6c39323789c4055d
Phil Barber <phil@lantum.com>
2020-07-03 09:33:25 +0100

    Move style

--- src/style.ts
+++ src/style.ts
@@ -4,7 +4,29 @@ const navy = '#001F3F';
 const orange = '#FF851B';
 const silver = '#ddd';
 
-export const theme = {
+interface Theme {
+  readonly colors: {
+    readonly primary: string;
+    readonly secondary: string;
+    readonly tertiary: string;
+    readonly black: string;
+    readonly white: string;
+  };
+  readonly spacing: {
+    readonly xxxSmall: string;
+    readonly xxSmall: string;
+    readonly xSmall: string;
+    readonly small: string;
+    readonly normal: string;
+    readonly medium: string;
+    readonly large: string;
+    readonly xLarge: string;
+    readonly xxLarge: string;
+    readonly xxxLarge: string;
+  };
+}
+
+export const theme: Theme = {
   colors: {
     primary: orange,
     secondary: navy,
